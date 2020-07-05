4c266353f4fbfc127e8d375998fc4cadf186c4d9
Pointsz <pfvfake123@gmail.com>
2020-07-05 18:17:52 -0300

    make it better

--- src/controllers/RestaurantController.js
+++ src/controllers/RestaurantController.js
@@ -1,13 +1,22 @@
 const db = require("../database");
 
 module.exports = {
-    async getRestaurantList() {
+    async getRestaurantList(req, res) {
         let doc = await db.Restaurant.find({}, { cnpj: 0, });
 
         res.json(doc);
     },
-    createRestaurant(req, res) {
+    async createRestaurant(req, res) {
         const value = req.body;
+
+        const existsRestaurantName = await db.Restaurant.exists({ company_name: value.company_name });
+        const existsRestaurantCnpj = await db.Restaurant.exists({ cnpj: value.cnpj });
+
+        if(existsRestaurantName || existsRestaurantCnpj) return res.json({ message: "Este restaurante já existe" })
+
+        if(!value.company_name && !value.cnpj && !value.address) {
+            return res.json({ message: "Há dados faltando." })
+        }
 
         let Restaurant = new db.Restaurant({
             company_name: value.company_name,
@@ -21,5 +30,7 @@ module.exports = {
         })
 
         Restaurant.save()
+
+        res.json({ message: "Sucesso" })
     }
 };
