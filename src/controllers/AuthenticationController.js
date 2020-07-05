26fa15c8a92fe46b4a8af813653e5fe504b7c956
Pointsz <pfvfake123@gmail.com>
2020-07-05 19:07:43 -0300

    make it better

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -29,7 +29,7 @@ module.exports = {
 
         userDb.save();
 
-        res.set("x-token", `Bearer ${jwt.sign({ id: userDb._id, name: userDb.name, email: userDb.email, points: userDb.points, reservations }, "shh")}`);
+        res.set("x-token", jwt.sign({ id: userDb._id, name: userDb.name, email: userDb.email, points: userDb.points, reservations }, "shh" ));
         res.json({ message: "Success" });
     },
     async logIn(req, res) {
@@ -43,7 +43,7 @@ module.exports = {
         const reservations = await db.Reservation.findOne({ owner_id: userDb._id }, { _id: 0});
 
         
-        res.set("x-token", `Bearer ${jwt.sign({ id: userDb._id,  name: userDb.name, email: userDb.email, points: userDb.points, reservations }, "shh")}`)
+        res.set("x-token", jwt.sign({ id: userDb._id,  name: userDb.name, email: userDb.email, points: userDb.points, reservations }, "shh"))
         res.json({ message: "Success" });
     }
 };
